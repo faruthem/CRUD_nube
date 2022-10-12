36e36bb6b7549a9b8128c698a745647765cb9afe
Tarsicio Ramos <t.ramos@ugto.mx>
2022-10-11 22:37:40 -0500

    Se colocaron las variables de entorno para darle seguridad a la APP  donde se coloco solamente el PORT y MONGODB_URI

--- src/databse.js
+++ src/databse.js
@@ -1,9 +1,11 @@
 import { connect } from "mongoose";
+import {MONGODB_URI} from'./config';
+
+
+
 (async()=>{
     try {
-        const db = await connect("mongodb+srv://usuario:clave@cluster0.2bbaoni.mongodb.net/crud")
-        //const db = await connect("mongodb://localhost/crud")
-        //const db = await connect("mongodb://mongo/crud")
+        const db = await connect(MONGODB_URI)
         console.log("BASE DE DATOS CONECTADA A", db.connection.name)
     } catch (error) {
         console.log(error);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import app from "./app";
 import './databse';
-app.listen(3000);
-console.log("SERVIDOR EN POUERTO", 3000);
+import {PORT} from './config';
+
+app.listen(PORT);
+console.log("SERVIDOR EN PUERTO", PORT);
