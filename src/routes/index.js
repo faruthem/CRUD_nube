ec514133d6c849ee128db9a79831034f5f6b93a2
Tarsicio Ramos <t.ramos@ugto.mx>
2022-10-11 22:09:23 -0500

    Se separo el condigo en modelo vista controlador, donde se agrego la carpeta controllers y se coloco la funcion de cada ruta

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,70 +1,16 @@
 import { Router } from "express"
 import Task from "../models/Task";
+import { renderTasks, createTask,aboutTask, renderTaskEdit, editTask, delteTask, doneTask } from "../controllers/tasks.controller";
 
 const router = Router();
 
-router.get("/",async(req,res)=>{
-    
-
-    const tasks = await Task.find().lean();
-    //console.log(tasks[0]);
-    res.render("Index",{tasks:tasks});
-});
-
-router.post("/tasks/add",async(req,res)=>{
-
-    try {
-        const task =Task(req.body)
-        const taskSaved = await task.save();
-        console.log(taskSaved);
-        //console.log(req.body);
-        //res.send("Guardar");
-        res.redirect('/');
-    } catch (error) {
-        console.log(error);
-    }
-
-    
-
-});
-
-router.get("/about",(req,res)=>{
-    res.render("about");
-});
-router.get("/edit/:id",async(req,res)=>{
-    //console.log(req.params.id)
-  const task = await Task.findById(req.params.id).lean()
-
-   // res.render("edit");
-    res.render("edit",{task});
-});
-
-router.post("/edit/:id",async(req,res)=>{
-    const{id } = req.params
-
-    await Task.findByIdAndUpdate(id, req.body)
-
-    //console.log(req.body);
-    //res.send('Cambio recibido');
-    res.redirect('/');
-});
-
-
-router.get("/delete/:id",async(req,res)=>{
-    const { id } = req.params;
-    await Task.findByIdAndDelete(id)
-    //res.render("delete");
-    res.redirect('/');
-});
-
-router.get("/taggdone/:id",async(req,res)=>{
-    const{id } = req.params;
-    const task = await Task.findById(id)
-    task.done = !task.done;
-    await task.save();
-    res.redirect("/");
-
-});
+router.get("/", renderTasks);
+router.post("/tasks/add",createTask) ;
+router.get("/about",aboutTask);
+router.get("/edit/:id",renderTaskEdit);
+router.post("/edit/:id",editTask);
+router.get("/delete/:id",delteTask);
+router.get("/taggdone/:id",doneTask);
 
 
 export default router;
